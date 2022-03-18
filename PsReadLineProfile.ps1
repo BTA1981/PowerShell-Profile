58ccd1502f7c89f5fe73c3c41a5e2190847a3614
Bart <bart_tacken@hotmail.com>
2022-03-18 11:24:01 +0100

    Create PsReadLineProfile.ps1

---
+++ PsReadLineProfile.ps1
@@ -0,0 +1,113 @@
+
+Import-Module PSReadLine
+
+# Searching for commands with up/down arrow is really handy. 
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Changing colors for history matches
+Set-PSReadLineOption -Colors @{emphasis = '#ff0000'; inlinePrediction = 'magenta'}
+
+# Import AZ predictor module
+Import-Module Az.Tools.Predictor
+
+# Set PsreadLine to predict based on cmd history and AI plugin
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+
+
+# This key handler shows the entire or filtered history using Out-GridView. The
+# typed text is used as the substring pattern for filtering. A selected command
+# is inserted to the command line without invoking. Multiple command selection
+# is supported, e.g. selected by Ctrl + Click.
+Set-PSReadLineKeyHandler -Key F7 `
+                         -BriefDescription History `
+                         -LongDescription 'Show command history' `
+                         -ScriptBlock {
+    $pattern = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
+    if ($pattern)
+    {
+        $pattern = [regex]::Escape($pattern)
+    }
+
+    $history = [System.Collections.ArrayList]@(
+        $last = ''
+        $lines = ''
+        foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath))
+        {
+            if ($line.EndsWith('`'))
+            {
+                $line = $line.Substring(0, $line.Length - 1)
+                $lines = if ($lines)
+                {
+                    "$lines`n$line"
+                }
+                else
+                {
+                    $line
+                }
+                continue
+            }
+
+            if ($lines)
+            {
+                $line = "$lines`n$line"
+                $lines = ''
+            }
+
+            if (($line -cne $last) -and (!$pattern -or ($line -match $pattern)))
+            {
+                $last = $line
+                $line
+            }
+        }
+    )
+    $history.Reverse()
+
+    $command = $history | Out-GridView -Title History -PassThru
+    if ($command)
+    {
+        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
+    }
+}
+
+# This example will replace any aliases on the command line with the resolved commands.
+Set-PSReadLineKeyHandler -Key "Alt+%" `
+                         -BriefDescription ExpandAliases `
+                         -LongDescription "Replace all aliases with the full command" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $ast = $null
+    $tokens = $null
+    $errors = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+
+    $startAdjustment = 0
+    foreach ($token in $tokens)
+    {
+        if ($token.TokenFlags -band [TokenFlags]::CommandName)
+        {
+            $alias = $ExecutionContext.InvokeCommand.GetCommand($token.Extent.Text, 'Alias')
+            if ($alias -ne $null)
+            {
+                $resolvedCommand = $alias.ResolvedCommandName
+                if ($resolvedCommand -ne $null)
+                {
+                    $extent = $token.Extent
+                    $length = $extent.EndOffset - $extent.StartOffset
+                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                        $extent.StartOffset + $startAdjustment,
+                        $length,
+                        $resolvedCommand)
+
+                    # Our copy of the tokens won't have been updated, so we need to
+                    # adjust by the difference in length
+                    $startAdjustment += ($resolvedCommand.Length - $length)
+                }
+            }
+        }
+    }
+}
